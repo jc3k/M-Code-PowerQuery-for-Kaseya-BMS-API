5866ef4f3576b9300d135198983a40aac867653e
JC <48335229+jc3k@users.noreply.github.com>
2023-07-07 08:37:47 -0500

    Update BMS_Status_List.m

--- BMS_Status_List.m
+++ BMS_Status_List.m
@@ -1,29 +1,31 @@
 let
     // Retrieve the access token from the previous query
-    access_token = #"Auth_Query",
+    access_token = BMS_Auth_Query,
 
-    // Define the API endpoint and request headers
-    url = "https://api.bms.kaseya.com/v2/system/statuses/",
+    // Define the base API endpoint and RelativePath
+    baseUrl = "https://api.bms.kaseya.com/",
+    relativePath = "v2/system/statuses/",
 
-    LoadData = (url as text, access_token as text, pageNumber as number, pageSize as number) =>
+    LoadData = (baseUrl as text, relativePath as text, access_token as text, pageNumber as number, pageSize as number) =>
     let
         queryParameters = [
             PageSize = Text.From(pageSize),
             pageNumber = Text.From(pageNumber)
         ],
         headers = [#"Authorization" = "Bearer " & access_token, #"Content-Type" = "application/json", #"Accept" = "application/json"],
-        response = Web.Contents(url, [Headers=headers, Query=queryParameters]),
+        options = [Headers=headers, Query=queryParameters, RelativePath=relativePath],
+        response = Web.Contents(baseUrl, options),
         jsonResponse = Json.Document(response),
         resultList = jsonResponse[result],
         data = Table.FromRecords(resultList),
         nextPageNumber = pageNumber + 1,
-        nextData = if Table.IsEmpty(data) then null else @LoadData(url, access_token, nextPageNumber, pageSize),
+        nextData = if Table.IsEmpty(data) then null else @LoadData(baseUrl, relativePath, access_token, nextPageNumber, pageSize),
         combinedData = if nextData = null then data else Table.Combine({data, nextData})
     in
         combinedData,
 
     // Initialize the recursive function with the starting page number and page size
-    table = LoadData(url, access_token, 0, 100)
+    table = LoadData(baseUrl, relativePath, access_token, 0, 100)
 
 in
     table
